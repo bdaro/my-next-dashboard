f64ba99861eabd8aaae09b9adfb3115d7d54843f
bdaro <104726487+bdaro@users.noreply.github.com>
2025-08-04 15:03:34 +0530

    remove duplicate code.

--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -4,9 +4,3 @@ export const lusitana = Lusitana({
   weight: ['400', '700'],
   subsets: ['latin'],
 });
-import { Inter, Lusitana } from 'next/font/google';
-export const inter = Inter({ subsets: ['latin'] });
-export const lusitana = Lusitana({
-  weight: ['400', '700'],
-  subsets: ['latin'],
-});
